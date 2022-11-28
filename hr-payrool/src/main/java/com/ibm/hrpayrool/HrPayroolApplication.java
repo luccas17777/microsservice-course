9832461dbce14f0ed3462302c7d9fbce96c57cc1
Luccas Eduardo <luccasalzira@gmail.com>
2022-11-28 15:33:13 -0300

    Feign Client

--- hr-payrool/src/main/java/com/ibm/hrpayrool/HrPayroolApplication.java
+++ hr-payrool/src/main/java/com/ibm/hrpayrool/HrPayroolApplication.java
@@ -2,7 +2,9 @@ package com.ibm.hrpayrool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class HrPayroolApplication {
 
